9e735c160927e54577e874848f0203adef19c458
jwilson4BC <124467835+jwilson4BC@users.noreply.github.com>
2024-10-14 16:39:37 -0400

    Add files via upload

---
+++ MS365-PScommands.ps1
@@ -0,0 +1,11 @@
+﻿#these command are to be used with M365 Adminitrsator Tenant
+#download M365 Module
+Install-Module -name MSonline
+#Add M365 Module to the Poewershell Commands
+Import-Module -name MSonline
+#show all M365 Commands
+get-command -module MSonline
+#connect you to your M365 tenant
+Connect-MSolservice
+#show Users
+get-MSonluser
